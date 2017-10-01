5b91194711023cfe0a10ef232a08512c94e7a5a7
Michael Royston <mindfire1@msn.com>
2017-10-01 15:56:24 -0400

    test
    test

--- public/assets/js/arteegee.js
+++ public/assets/js/arteegee.js
@@ -36,7 +36,7 @@
                     break
 								case 'drew':
 										console.log('oppenent drew card ' + data[1] + ' cards remain')
-										$('#game .enemyCardsLeft').value(data[1])
+										$('#game .enemyCardsLeft').val(data[1])
 										if (data[1] <= 0) {
 											wonGame()
 										}
@@ -195,6 +195,7 @@
 		    deck[i] = new Card(i)
 		}
 		console.log("Deck size: " + deck.length)
+		$('#game .myCardsLeft').val(deck.length)
 
 		peer = new Peer('', {
 			host: location.hostname,
@@ -314,7 +315,7 @@ console.log('Chatting happened');
 				if(deck.length == 0) {
 					lostGame()
 				}
-				$('#game .myCardsLeft').value(deck.length)
+				$('#game .myCardsLeft').val(deck.length)
 				console.log('Current Card Name: ' + currentCard.getCardName())
 				console.log('New Deck Size: ' + deck.length)
     })
